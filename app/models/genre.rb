356ff78315e86a9ad90408fd5c2c14bdf2eb9cdc
Melissa Guachun <1212malka@gmail.com>
2021-03-23 14:07:22 -0400

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,18 @@
 class Genre < ActiveRecord::Base
     has_many :songs
     has_many :artists, through: :songs
+
+    def song_count
+        self.songs.length
+    end
+
+    def artist_count
+        self.artists.length
+    end
+
+    def all_artist_name
+        self.artists.map(&:name)
+    end
+
+
 end
